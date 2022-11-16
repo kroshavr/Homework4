95779cbc07e125d1652077be736af0d8a462fe99
Костя <kroshavr@mail.ru>
2022-11-16 11:55:02 +0500

    Homework

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,30 @@ public class Main {
         int age = 7;
         if (age >= 18) {
             System.out.println("Поздравляем с совершеннолетием!");
-        }
-        if (age < 18) {
+        } else {
             System.out.println("Вам еще нет 18 лет!");
         }
         //Задание 2
-        int age2 = 19;
-        if (age2 >= 7) {
+        int age2 = 26;
+        if (age2 >= 7 && age2 < 18) {
             System.out.println("Ребенок ходит в школу");
         }
-        if (age2 >= 18) {
+        else if (age2 >=18 && age2 < 24) {
             System.out.println("Человек учится в университете");
         }
-        if (age2 >= 24) {
+        else {
             System.out.println("Человек работает");
         }
+
         //Задание 3
-        int people = 101;
-        int capacity = 102;
-        int seatPlace = 60;
-        int standingPlace = capacity - seatPlace;
+        int people = 105;
         if (people < 60) {
             System.out.println("В вагоне есть сидячие и стоячие места");
         }
-        if (people < 102) {
+        else if (people >= 60 && people < 102) {
             System.out.println("В вагоне есть стоячие места");
         }
-        if (people >=102) {
+        else {
             System.out.println("В вагоне нет мест");
         }
     }
