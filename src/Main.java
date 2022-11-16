159233023139f75f08fc635975086ff58c457983
Костя <kroshavr@mail.ru>
2022-11-16 11:04:08 +0500

    Homework

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание 1
+        int age = 7;
+        if (age >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
+        if (age < 18) {
+            System.out.println("Вам еще нет 18 лет!");
+        }
+        //Задание 2
+        int age2 = 19;
+        if (age2 >= 7) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (age2 >= 18) {
+            System.out.println("Человек учится в университете");
+        }
+        if (age2 >= 24) {
+            System.out.println("Человек работает");
+        }
+
+    }
+}
